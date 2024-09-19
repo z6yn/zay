d85b6f94a9cf4cc8f55f858b20e4415186be4633
z6yn <zayy.cloudd@gmail.com>
2024-09-19 22:31:43 +0530

    v2.3

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   content: [
-    './index.html', // Include all HTML files in the public folder
+    './index.html',
+    './images/airzy.jpg', // Include all HTML files in the public folder
     './src/tailwind.config.js',
     './src/postcss.config.js',        // Include all JS files in the src folder (optional if you use JS with Tailwind)
   ],
