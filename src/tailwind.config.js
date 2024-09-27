7c5c7a76559e2cab5a0858368772f7a53cd3fed2
z6yn <zayy.cloudd@gmail.com>
2024-09-27 21:41:16 +0530

    v3

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -1,7 +1,6 @@
 module.exports = {
-  purge: [
-    './index.html',
-    './images/airzy.jpg', // Include all HTML files in the public folder
+  content: [
+    './index.html', // Include all HTML files in the public folder
     './src/tailwind.config.js',
     './src/postcss.config.js',
     './src/styles.css',        // Include all JS files in the src folder (optional if you use JS with Tailwind)
